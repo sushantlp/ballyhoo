c51a76dfe5eec36961da5e01ed7e2585fb3726c2
sushantlp <sushantsingh.1081@gmail.com>
2018-09-13 14:03:33 +0530

    Book component ui finished

--- src/components/detailViewComponents/bookComponent/book.js
+++ src/components/detailViewComponents/bookComponent/book.js
@@ -1,11 +1,10 @@
 import React from "react";
 
 import {
-  Card,
-  Container,
   Segment,
-  Rating,
-  Button
+  Button,
+  Divider,
+  Icon
 } from "semantic-ui-react/dist/commonjs";
 
 import classes from "./static/css/book.css";
@@ -18,14 +17,85 @@ export default class Book extends React.Component {
   render() {
     return (
       <div>
-        <Segment style={{ width: "400px", height: "400px" }}>
+        <Segment style={{ width: "400px" }}>
           <label
             style={{
-              fontSize: "14px"
+              fontSize: "20px",
+              fontWeight: "bold",
+              color: "rgba(0,0,0,.9)"
             }}
           >
             ₹9,077
           </label>
+
+          <Divider />
+
+          <span
+            style={{
+              marginLeft: "24px",
+              marginRight: "24px"
+            }}
+          >
+            <label
+              style={{
+                fontSize: "22px",
+                color: "rgba(0,0,0,.6)"
+              }}
+            >
+              Quantity
+            </label>
+
+            <Icon
+              disabled
+              name="minus square outline"
+              style={{
+                fontSize: "25px",
+                marginLeft: "20px"
+              }}
+            />
+            <label
+              style={{
+                fontSize: "22px",
+                paddingLeft: "10px",
+                paddingRight: "10px"
+              }}
+            >
+              1
+            </label>
+            <Icon
+              disabled
+              name="plus square outline"
+              style={{
+                fontSize: "25px"
+              }}
+            />
+          </span>
+
+          <Button
+            style={{
+              backgroundColor: "#FF5A5F",
+              color: "white",
+              opacity: "1",
+              width: "320px",
+              height: "50px",
+              fontSize: "20px",
+              fontWeight: "500",
+              marginTop: "20px",
+              marginLeft: "24px",
+              marginRight: "24px"
+            }}
+          >
+            Procced
+          </Button>
+          <p
+            style={{
+              marginLeft: "100px",
+              marginRight: "100px",
+              marginTop: "5px"
+            }}
+          >
+            You won’t be charged yet
+          </p>
         </Segment>
       </div>
     );
