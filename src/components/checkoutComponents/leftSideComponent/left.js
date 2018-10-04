25912da94491a9860e7401971e5c965ab60e7f9c
sushantlp <sushantsingh.1081@gmail.com>
2018-10-04 19:00:06 +0530

    checkout left component break

--- src/components/checkoutComponents/leftSideComponent/left.js
+++ src/components/checkoutComponents/leftSideComponent/left.js
@@ -15,6 +15,213 @@ export default class Left extends React.Component {
     super(props);
   }
 
+  firstHalfComponent = () => {
+    return (
+      <div>
+        {" "}
+        <label
+          style={{
+            color: "rgba(0,0,0,.6)",
+            fontSize: "14px",
+            lineHeight: "20px"
+          }}
+        >
+          Your order from
+        </label>
+        <div>
+          <label
+            style={{
+              fontSize: "22px",
+              lineHeight: "26px"
+            }}
+          >
+            DELIBOX
+          </label>
+        </div>
+        <Button
+          style={{
+            backgroundColor: "#FF5A5F",
+            color: "white",
+            opacity: "1",
+            width: "320px",
+            height: "50px",
+            fontSize: "20px",
+            fontWeight: "500",
+            marginTop: "20px",
+            marginLeft: "24px",
+            marginRight: "24px"
+          }}
+        >
+          Place Order
+        </Button>
+      </div>
+    );
+  };
+
+  secondHalfComponent = () => {
+    return (
+      <div>
+        {" "}
+        <label
+          style={{
+            fontSize: "20px"
+          }}
+        >
+          Lunch Buffet
+        </label>
+        <span
+          style={{
+            position: "absolute",
+            left: "190px"
+          }}
+        >
+          <Icon
+            name="minus square outline"
+            style={{
+              color: "rgba(0,0,0,.6)",
+              fontSize: "18px",
+              display: "inline",
+              cursor: "pointer"
+            }}
+          />
+          <label
+            style={{
+              fontSize: "16px",
+              paddingLeft: "5px",
+              paddingRight: "7px",
+              display: "inline"
+            }}
+          >
+            2
+          </label>
+          <Icon
+            name="plus square outline"
+            style={{
+              color: "rgba(0,0,0,.6)",
+              fontSize: "18px",
+              display: "inline",
+              cursor: "pointer"
+            }}
+          />
+        </span>
+        <span
+          style={{
+            float: "right"
+          }}
+        >
+          <label
+            style={{
+              fontSize: "20px"
+            }}
+          >
+            $200
+          </label>
+        </span>
+      </div>
+    );
+  };
+
+  thirdHalfComponent = () => {
+    return;
+  };
+
+  fourthHalfComponent = () => {
+    return (
+      <div>
+        <div
+          style={{
+            color: "rgba(0,0,0,.6)",
+            fontSize: "14px",
+            lineHeight: "20px",
+            justifyContent: "space-between"
+          }}
+        >
+          <label>Subtotal</label>
+
+          <label
+            style={{
+              float: "right"
+            }}
+          >
+            $200
+          </label>
+        </div>
+        <div
+          style={{
+            color: "rgba(0,0,0,.6)",
+            fontSize: "14px",
+            lineHeight: "20px",
+            justifyContent: "space-between"
+          }}
+        >
+          <label>Service Charge</label>
+
+          <label
+            style={{
+              float: "right"
+            }}
+          >
+            $50
+          </label>
+        </div>
+        <div
+          style={{
+            color: "rgba(0,0,0,.6)",
+            fontSize: "14px",
+            lineHeight: "20px",
+            justifyContent: "space-between"
+          }}
+        >
+          <label>Cgst</label>
+
+          <label
+            style={{
+              float: "right"
+            }}
+          >
+            $9
+          </label>
+        </div>
+        <div
+          style={{
+            color: "rgba(0,0,0,.6)",
+            fontSize: "14px",
+            lineHeight: "20px",
+            justifyContent: "space-between"
+          }}
+        >
+          <label>Sgst</label>
+
+          <label
+            style={{
+              float: "right"
+            }}
+          >
+            $9
+          </label>
+        </div>
+        <div
+          style={{
+            fontSize: "14px",
+            lineHeight: "20px",
+            fontWeight: "bold",
+            justifyContent: "space-between",
+            paddingTop: "10px"
+          }}
+        >
+          <label>Total</label>
+
+          <label
+            style={{
+              float: "right"
+            }}
+          >
+            $269
+          </label>
+        </div>
+      </div>
+    );
+  };
   render() {
     if (this.props.parentState.delivery) {
       if (this.props.deliveryAdditionalCharge.status === "START") {
@@ -29,198 +236,15 @@ export default class Left extends React.Component {
     return (
       <div>
         <Segment style={{ width: "400px" }}>
-          <label
-            style={{
-              color: "rgba(0,0,0,.6)",
-              fontSize: "14px",
-              lineHeight: "20px"
-            }}
-          >
-            Your order from
-          </label>
-          <div>
-            <label
-              style={{
-                fontSize: "22px",
-                lineHeight: "26px"
-              }}
-            >
-              DELIBOX
-            </label>
-          </div>
-          <Button
-            style={{
-              backgroundColor: "#FF5A5F",
-              color: "white",
-              opacity: "1",
-              width: "320px",
-              height: "50px",
-              fontSize: "20px",
-              fontWeight: "500",
-              marginTop: "20px",
-              marginLeft: "24px",
-              marginRight: "24px"
-            }}
-          >
-            Place Order
-          </Button>
+          {this.firstHalfComponent()}
 
           <Divider />
 
-          <label
-            style={{
-              fontSize: "20px"
-            }}
-          >
-            Lunch Buffet
-          </label>
-
-          <span
-            style={{
-              position: "absolute",
-              left: "190px"
-            }}
-          >
-            <Icon
-              name="minus square outline"
-              style={{
-                color: "rgba(0,0,0,.6)",
-                fontSize: "18px",
-                display: "inline",
-                cursor: "pointer"
-              }}
-            />
-            <label
-              style={{
-                fontSize: "16px",
-                paddingLeft: "5px",
-                paddingRight: "7px",
-                display: "inline"
-              }}
-            >
-              2
-            </label>
-            <Icon
-              name="plus square outline"
-              style={{
-                color: "rgba(0,0,0,.6)",
-                fontSize: "18px",
-                display: "inline",
-                cursor: "pointer"
-              }}
-            />
-          </span>
-
-          <span
-            style={{
-              float: "right"
-            }}
-          >
-            <label
-              style={{
-                fontSize: "20px"
-              }}
-            >
-              $200
-            </label>
-          </span>
+          {this.secondHalfComponent()}
           <Divider />
-
-          <div
-            style={{
-              color: "rgba(0,0,0,.6)",
-              fontSize: "14px",
-              lineHeight: "20px",
-              justifyContent: "space-between"
-            }}
-          >
-            <label>Subtotal</label>
-
-            <label
-              style={{
-                float: "right"
-              }}
-            >
-              $200
-            </label>
-          </div>
-
-          <div
-            style={{
-              color: "rgba(0,0,0,.6)",
-              fontSize: "14px",
-              lineHeight: "20px",
-              justifyContent: "space-between"
-            }}
-          >
-            <label>Service Charge</label>
-
-            <label
-              style={{
-                float: "right"
-              }}
-            >
-              $50
-            </label>
-          </div>
-
-          <div
-            style={{
-              color: "rgba(0,0,0,.6)",
-              fontSize: "14px",
-              lineHeight: "20px",
-              justifyContent: "space-between"
-            }}
-          >
-            <label>Cgst</label>
-
-            <label
-              style={{
-                float: "right"
-              }}
-            >
-              $9
-            </label>
-          </div>
-
-          <div
-            style={{
-              color: "rgba(0,0,0,.6)",
-              fontSize: "14px",
-              lineHeight: "20px",
-              justifyContent: "space-between"
-            }}
-          >
-            <label>Sgst</label>
-
-            <label
-              style={{
-                float: "right"
-              }}
-            >
-              $9
-            </label>
-          </div>
-
-          <div
-            style={{
-              fontSize: "14px",
-              lineHeight: "20px",
-              fontWeight: "bold",
-              justifyContent: "space-between",
-              paddingTop: "10px"
-            }}
-          >
-            <label>Total</label>
-
-            <label
-              style={{
-                float: "right"
-              }}
-            >
-              $269
-            </label>
-          </div>
+          {this.thirdHalfComponent()}
+          <Divider />
+          {this.fourthHalfComponent()}
         </Segment>
       </div>
     );
