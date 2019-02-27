0741b38232c2d594d5862684ded0597077610994
Suraj Sharma <www.surajsharma@gmail.com>
2019-02-27 14:34:33 +0530

    fixed word wrap in checkout left.js

--- src/components/checkoutComponents/leftSideComponent/left.js
+++ src/components/checkoutComponents/leftSideComponent/left.js
@@ -531,7 +531,7 @@ export default class Left extends React.Component {
               >
                 <label
                   style={{
-                    fontSize: "18px"
+                    fontSize: "18px",
                     width: "150px"
                   }}
                 >
